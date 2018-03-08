3bbe722bb38cb24dba90e7e2e64ac7308b102bf1
root <root@localhost.localdomain>
2018-03-08 17:48:59 -0500

    first commit

---
+++ CMakeFiles/xmrig.dir/DependInfo.cmake
@@ -0,0 +1,92 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/Desktop/xmrig/src/crypto/c_blake256.c" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_blake256.c.o"
+  "/root/Desktop/xmrig/src/crypto/c_groestl.c" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_groestl.c.o"
+  "/root/Desktop/xmrig/src/crypto/c_jh.c" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_jh.c.o"
+  "/root/Desktop/xmrig/src/crypto/c_keccak.c" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_keccak.c.o"
+  "/root/Desktop/xmrig/src/crypto/c_skein.c" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/c_skein.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_SYSLOG_H"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/3rdparty/libcpuid"
+  "../src"
+  "../src/3rdparty"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/Desktop/xmrig/src/App.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/App.cpp.o"
+  "/root/Desktop/xmrig/src/App_unix.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/App_unix.cpp.o"
+  "/root/Desktop/xmrig/src/Console.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/Console.cpp.o"
+  "/root/Desktop/xmrig/src/Cpu.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/Cpu.cpp.o"
+  "/root/Desktop/xmrig/src/Cpu_unix.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/Cpu_unix.cpp.o"
+  "/root/Desktop/xmrig/src/Mem.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/Mem.cpp.o"
+  "/root/Desktop/xmrig/src/Mem_unix.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/Mem_unix.cpp.o"
+  "/root/Desktop/xmrig/src/Options.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/Options.cpp.o"
+  "/root/Desktop/xmrig/src/Platform.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/Platform.cpp.o"
+  "/root/Desktop/xmrig/src/Platform_unix.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/Platform_unix.cpp.o"
+  "/root/Desktop/xmrig/src/Summary.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/Summary.cpp.o"
+  "/root/Desktop/xmrig/src/api/Api.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/api/Api.cpp.o"
+  "/root/Desktop/xmrig/src/api/ApiState.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/api/ApiState.cpp.o"
+  "/root/Desktop/xmrig/src/api/Httpd.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/api/Httpd.cpp.o"
+  "/root/Desktop/xmrig/src/api/NetworkState.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/api/NetworkState.cpp.o"
+  "/root/Desktop/xmrig/src/crypto/CryptoNight.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/crypto/CryptoNight.cpp.o"
+  "/root/Desktop/xmrig/src/log/ConsoleLog.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/log/ConsoleLog.cpp.o"
+  "/root/Desktop/xmrig/src/log/FileLog.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/log/FileLog.cpp.o"
+  "/root/Desktop/xmrig/src/log/Log.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/log/Log.cpp.o"
+  "/root/Desktop/xmrig/src/log/SysLog.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/log/SysLog.cpp.o"
+  "/root/Desktop/xmrig/src/net/Client.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/net/Client.cpp.o"
+  "/root/Desktop/xmrig/src/net/Job.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/net/Job.cpp.o"
+  "/root/Desktop/xmrig/src/net/Network.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/net/Network.cpp.o"
+  "/root/Desktop/xmrig/src/net/SubmitResult.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/net/SubmitResult.cpp.o"
+  "/root/Desktop/xmrig/src/net/Url.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/net/Url.cpp.o"
+  "/root/Desktop/xmrig/src/net/strategies/DonateStrategy.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/net/strategies/DonateStrategy.cpp.o"
+  "/root/Desktop/xmrig/src/net/strategies/FailoverStrategy.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/net/strategies/FailoverStrategy.cpp.o"
+  "/root/Desktop/xmrig/src/net/strategies/SinglePoolStrategy.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/net/strategies/SinglePoolStrategy.cpp.o"
+  "/root/Desktop/xmrig/src/workers/DoubleWorker.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/workers/DoubleWorker.cpp.o"
+  "/root/Desktop/xmrig/src/workers/Handle.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Handle.cpp.o"
+  "/root/Desktop/xmrig/src/workers/Hashrate.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Hashrate.cpp.o"
+  "/root/Desktop/xmrig/src/workers/SingleWorker.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/workers/SingleWorker.cpp.o"
+  "/root/Desktop/xmrig/src/workers/Worker.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Worker.cpp.o"
+  "/root/Desktop/xmrig/src/workers/Workers.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/workers/Workers.cpp.o"
+  "/root/Desktop/xmrig/src/xmrig.cpp" "/root/Desktop/xmrig/build/CMakeFiles/xmrig.dir/src/xmrig.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_SYSLOG_H"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/3rdparty/libcpuid"
+  "../src"
+  "../src/3rdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/Desktop/xmrig/build/src/3rdparty/libcpuid/CMakeFiles/cpuid.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
